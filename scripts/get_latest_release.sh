81594b85b0f0583d87170301bbf53f3a84cb7142
ILoveYaToo <dev@iloveyatoo.com>
2022-03-20 01:39:29 +0000

    Initial Commit of semaphore

---
+++ scripts/get_latest_release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Check Latest Release
+LATEST_RELEASE=$(curl -Ls 'https://api.github.com/repos/pgbouncer/pgbouncer.github.io/contents/downloads/files' | jq -r '[.[].name] | sort_by( values | split(".") | map(tonumber) ) | .[-1]')
+
+## Build image if not on docker hub
+if docker manifest inspect iloveyatoo/pgbouncer:$LATEST_RELEASE >/dev/null; then
+    echo "Image already exists"
+else
+    cd ~
+    docker build -t iloveyatoo/pgbouncer:$LATEST_RELEASE
+    docker push iloveyatoo/pgbouncer:$LATEST_RELEASE
+fi
